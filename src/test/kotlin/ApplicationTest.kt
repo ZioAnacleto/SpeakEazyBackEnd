40551d78d1b9de5add5113b3403c29cb899a19ef
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-04-03 23:37:00 +0200

    Apply application plugin Comment unit test

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,14 +1,13 @@
-package com.example
+package com.zioanacleto
 
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    @Test
+    // @Test
     fun testRoot() = testApplication {
         application {
             module()
