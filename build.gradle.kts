7fe27cf3337debec6766515778d3933438aa4031
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-04-03 23:46:52 +0200

    Force gradle to add Main-Class in generated manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,7 @@ group = "com.zioanacleto"
 version = "0.0.1"
 
 application {
-    mainClass = "com.zioanacleto.ApplicationKt"
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    mainClass.set("com.zioanacleto.ApplicationKt")
 }
 
 repositories {
@@ -35,4 +32,10 @@ dependencies {
 
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "com.zioanacleto.ApplicationKt"  // Assicurati che il nome sia corretto
+    }
 }
