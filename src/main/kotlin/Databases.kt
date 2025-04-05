086b65c0f105496e1dafbb04ae1731f97a2757cb
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-04-05 22:01:59 +0200

    Delete sensitive data to connect to database

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Database
 
 fun configureDatabases(): Database = Database.connect(
     url = "jdbc:postgresql://postgres.railway.internal:5432/railway",
-    user = System.getenv("PGUSER") ?: "postgres",
+    user = System.getenv("PGUSER"),
     driver = "org.postgresql.Driver",
-    password = System.getenv("PGPASSWORD") ?: "yzlDCZGdcwnFtZXnfINcBKxuzwaeUiXs"
+    password = System.getenv("PGPASSWORD")
 )
