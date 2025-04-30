7ab29a4bbfb8557a54456838181cc9d92fcb1e6e
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-05-01 00:17:27 +0200

    Revert "Refactor home service to speed it up"
    This reverts commit 1040a975515435a75b360da47646d664bafacc13.

--- src/main/kotlin/home/HomeSchema.kt
+++ src/main/kotlin/home/HomeSchema.kt
@@ -2,46 +2,35 @@ package com.zioanacleto.home
 
 import com.zioanacleto.cocktails.CocktailService
 import com.zioanacleto.dbQuery
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import org.jetbrains.exposed.sql.Database
 
-class HomeService(private val database: Database) {
+class HomeService(val database: Database) {
 
     suspend fun homeSections(): ExposedHomeSectionsList = dbQuery {
         var id = 0
-        coroutineScope {
-            val deferredUnforgettables = async(Dispatchers.IO) {
-                CocktailService(database).readAllWithCategory(QUERY_COCKTAIL_CATEGORY).cocktails
-            }.await()
-            val deferredBeforeDinners = async(Dispatchers.IO) {
-                CocktailService(database).readAllWithType(QUERY_COCKTAIL_TYPE).cocktails
-            }.await()
-            val deferredMostPopular = async(Dispatchers.IO) {
-                CocktailService(database).readMostPopular(QUERY_COCKTAIL_POPULAR_SIZE).cocktails
-            }.await()
+        val unforgettables = CocktailService(database).readAllWithCategory(QUERY_COCKTAIL_CATEGORY).cocktails
+        val beforeDinners = CocktailService(database).readAllWithType(QUERY_COCKTAIL_TYPE).cocktails
+        val mostPopular = CocktailService(database).readMostPopular(QUERY_COCKTAIL_POPULAR_SIZE).cocktails
 
-            ExposedHomeSectionsList(
-                listOf(
-                    ExposedHomeSection(
-                        id = (++id).toString(),
-                        name = "IBA: The Unforgettables",
-                        cocktails = deferredUnforgettables
-                    ),
-                    ExposedHomeSection(
-                        id = (++id).toString(),
-                        name = "Before Dinner Cocktails",
-                        cocktails = deferredBeforeDinners
-                    ),
-                    ExposedHomeSection(
-                        id = (++id).toString(),
-                        name = "I più popolari",
-                        cocktails = deferredMostPopular
-                    )
+        ExposedHomeSectionsList(
+            listOf(
+                ExposedHomeSection(
+                    id = (++id).toString(),
+                    name = "IBA: The Unforgettables",
+                    cocktails = unforgettables
+                ),
+                ExposedHomeSection(
+                    id = (++id).toString(),
+                    name = "Before Dinner Cocktails",
+                    cocktails = beforeDinners
+                ),
+                ExposedHomeSection(
+                    id = (++id).toString(),
+                    name = "I più popolari",
+                    cocktails = mostPopular
                 )
             )
-        }
+        )
     }
 
     companion object {
