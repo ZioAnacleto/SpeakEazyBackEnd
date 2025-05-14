9c67c5fb714f619afa3bfebc2f47368991a7082e
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-05-14 23:40:55 +0200

    Add double api call to better help AI model to understand prompt

--- src/main/kotlin/search/SearchSchema.kt
+++ src/main/kotlin/search/SearchSchema.kt
@@ -12,6 +12,8 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
 
@@ -23,60 +25,42 @@ class SearchService(private val database: Database) {
             val ingredientsService = IngredientsService(database)
             val tagsService = TagsService(database)
 
+            // retrieve data from DB (cocktails, ingredients, tags)
             val cocktails = cocktailsService.readAll()
             val ingredients = ingredientsService.readAll()
             val tags = tagsService.readAll()
 
             println("Search service, request: $prompt")
-            val client = HttpClient(CIO) {
-                install(ContentNegotiation) {
-                    json()
-                }
-            }
 
-            val url = requireNotNull(System.getenv("AI_URL")) {
-                "AI_URL not found in environment"
-            }
-            val token = requireNotNull(System.getenv("AI_TOKEN")) {
-                "AI_TOKEN not found in environment"
-            }
+            // perform two different api calls in parallel
+            val tagsResponse = performHuggingFaceApiCall(
+                prompt.query,
+                tags.tags.map { it.name }
+            )
 
-            val rawResponse = client.post(url) {
-                headers {
-                    append(HttpHeaders.Authorization, "Bearer $token")
-                }
-                contentType(ContentType.Application.Json)
-                setBody(
-                    HuggingFaceSearchRequest(
-                        inputs = prompt.query,
-                        parameters = Parameters(
-                            candidate_labels = tags.tags.map { it.name } + ingredients.ingredients.map { it.name }
-                        )
-                    )
-                )
-                timeout {
-                    requestTimeoutMillis = 30_000
-                }
-            }.bodyAsText()
+            println("Search api called, tagsResponse: $tagsResponse")
 
-            println("Raw AI response: $rawResponse")
-            client.close()
+            val ingredientsResponse = performHuggingFaceApiCall(
+                prompt.query,
+                ingredients.ingredients.map { it.name }
+            )
 
-            val response = Json.decodeFromString<SearchResponse>(rawResponse)
+            println("Search api called, ingredientsResponse: $ingredientsResponse")
 
-            println("Search api called, embeddings result: $response")
+            // select tags and ingredients over threshold, that match the prompt the most
+            val tagsOverThreshold = tagsResponse.computeAcceptableLabels()
+            val ingredientsOverThreshold = ingredientsResponse.computeAcceptableLabels()
 
-            val tagsThatMatch = response.labels.zip(response.scores)
-                .filter { (_, score) -> score > SCORE_THRESHOLD }
-                .map { (label, _) -> label }
+            println("Tags that are over threshold: $tagsOverThreshold")
+            println("Ingredients that are over threshold: $ingredientsOverThreshold")
 
-            println("Tags that are over threshold: $tagsThatMatch")
-
+            // find ingredients that match
             val ingredientsThatMatch = ingredients.ingredients
-                .filter { ingredient -> tagsThatMatch.any { it == ingredient.name } }
+                .filter { ingredient -> ingredientsOverThreshold.any { it == ingredient.name } }
 
             println("Ingredients contained in labels: $ingredientsThatMatch")
 
+            // find cocktail using those ingredients
             val cocktailsThatMatchIngredients = cocktails.cocktails
                 .filter {
                     it.ingredients.ingredients.any { cocktailIngredient ->
@@ -94,6 +78,56 @@ class SearchService(private val database: Database) {
             throw exception
         }
     }
+
+    private suspend fun performHuggingFaceApiCall(
+        prompt: String,
+        candidateLabels: List<String>
+    ): SearchResponse {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val url = requireNotNull(System.getenv("AI_URL")) {
+            "AI_URL not found in environment"
+        }
+        val token = requireNotNull(System.getenv("AI_TOKEN")) {
+            "AI_TOKEN not found in environment"
+        }
+
+        return coroutineScope {
+            async {
+                val rawResponse = client.post(url) {
+                    headers {
+                        append(HttpHeaders.Authorization, "Bearer $token")
+                    }
+                    contentType(ContentType.Application.Json)
+                    setBody(
+                        HuggingFaceSearchRequest(
+                            inputs = prompt,
+                            parameters = Parameters(
+                                candidate_labels = candidateLabels
+                            )
+                        )
+                    )
+                    timeout {
+                        requestTimeoutMillis = 30_000
+                    }
+                }.bodyAsText()
+
+                println("Raw AI response: $rawResponse")
+                client.close()
+
+                Json.decodeFromString<SearchResponse>(rawResponse)
+            }
+        }.await()
+    }
+
+    private fun SearchResponse.computeAcceptableLabels() =
+        labels.zip(this.scores)
+        .filter { (_, score) -> score > SCORE_THRESHOLD }
+        .map { (label, _) -> label }
 
     companion object {
         private const val SCORE_THRESHOLD = 0.30
