b9d0c85247b3886b103817d5ed083f080493057b
Fabrizio Traversa <traversa.fabrizio@gmail.com>
2025-12-15 23:23:08 +0100

    Parallelize queries to db for homeSections api call

--- src/main/kotlin/home/HomeSchema.kt
+++ src/main/kotlin/home/HomeSchema.kt
@@ -1,12 +1,14 @@
 package com.zioanacleto.home
 
 import com.zioanacleto.cocktails.CocktailService
-import com.zioanacleto.dbQuery
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.jetbrains.exposed.sql.Database
 
 class HomeService(private val database: Database) {
 
-    suspend fun homeSections(): ExposedHomeSectionsList = dbQuery {
+    suspend fun homeSections(): ExposedHomeSectionsList = coroutineScope {
         val config = HomeConfigManager.loadConfig()
         // take [numberOfSections] sections shuffled (including banners)
         val allSections = config.sections.shuffled().take(config.numberOfSections)
@@ -16,20 +18,24 @@ class HomeService(private val database: Database) {
 
         var id = 0
         // mapping all sections but banner
-        val exposedSections = allSections.filterOutBanner().map { section ->
-            val cocktails = when (section.type.toType()) {
-                SectionType.CATEGORY -> cocktailService.readAllWithCategory(section.query!!).cocktails
-                SectionType.TYPE -> cocktailService.readAllWithType(section.query!!).cocktails
-                SectionType.POPULAR -> cocktailService.readMostPopular(section.limit!!).cocktails
-                SectionType.INGREDIENT -> cocktailService.readAllWithIngredient(section.query!!).cocktails
-                else -> emptyList()
+        val sectionDeferreds = allSections.filterOutBanner().map { section ->
+            async {
+                val cocktails = when (section.type.toType()) {
+                    SectionType.CATEGORY -> cocktailService.readAllWithCategory(section.query!!).cocktails
+                    SectionType.TYPE -> cocktailService.readAllWithType(section.query!!).cocktails
+                    SectionType.POPULAR -> cocktailService.readMostPopular(section.limit!!).cocktails
+                    SectionType.INGREDIENT -> cocktailService.readAllWithIngredient(section.query!!).cocktails
+                    else -> emptyList()
+                }
+                ExposedHomeSection(
+                    id = (++id).toString(),
+                    name = section.title,
+                    cocktails = cocktails
+                )
             }
-            ExposedHomeSection(
-                id = (++id).toString(),
-                name = section.title,
-                cocktails = cocktails
-            )
         }
+
+        val exposedSections = sectionDeferreds.awaitAll()
         // mapping banner if available
         val banner = bannerSection?.let { section ->
             val cocktail = cocktailService.readSingleWithName(section.query!!)
